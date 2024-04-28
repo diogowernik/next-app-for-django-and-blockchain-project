ea3a8c25715db01b9efd3149bdcfe8b342178059
Diogo Wernik <11983260+diogowernik@users.noreply.github.com>
2024-04-27 23:46:09 -0300

    limpando e arrumando

--- arquivo/useDjangoAuth.js
+++
@@ -1,9 +0,0 @@
-// src/hooks/useDjangoAuth.js
-import { useContext } from 'react';
-import DjangoAuthContext from '@/context/DjangoContext';
-
-export const useDjangoAuth = () => {
-    const context = useContext(DjangoAuthContext);
-    if (!context) throw new Error('useDjangoAuth must be used within a DjangoProvider');
-    return context;
-};
